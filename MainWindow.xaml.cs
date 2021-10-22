31f8dcde205144474a520faf98bf4e6793fe6789
RomainLefranc <lefranc46@gmail.com>
2021-10-22 11:38:19 +0400

    Ajout git add / fit push / git commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,27 +18,22 @@ namespace labo
 
         private void CloneRepo(object sender, RoutedEventArgs e)
         {
-            int error = 0;
             if (folderPath.Text == "") {
-                error++;
-                MessageBox.Show("Veuillez saisir le chemin du dossier");
+                MessageBox.Show("Veuillez saisir le chemin d'accès au dossier");
+                return;
             }
             if (repoPath.Text == "") {
-                error++;
                 MessageBox.Show("Veuillez saisir le lien d'un repertoire Git");
-            }
-            if (error == 0)
-            {
-
-                Process proc = new Process();
-                proc.StartInfo.FileName = "cmd.exe";
-                proc.StartInfo.Arguments = "/C cd " + folderPath.Text + "&& git clone " + repoPath.Text;
-                proc.StartInfo.CreateNoWindow = true;
-                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                proc.Start();
-                proc.WaitForExit();
+                return;
             }
 
+            Process proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "/C cd " + folderPath.Text + "&& git clone " + repoPath.Text;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.Start();
+            proc.WaitForExit();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -48,6 +43,65 @@ namespace labo
             {
                 folderPath.Text = dialog.SelectedPath;
             }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (folderPath.Text != "")
+            {
+                MessageBox.Show("Veuillez saisir un chemin d'accès");
+                return;
+            }
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "/C cd " + folderPath.Text + "&& git add .";
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            if (CommitMessage.Text != "")
+            {
+                MessageBox.Show("Veuillez saisir un message commit");
+                return;
+            }
+
+            if (folderPath.Text != "")
+            {
+                MessageBox.Show("Veuillez saisir un chemin d'accès");
+                return;
+            }
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "/C cd " + folderPath.Text + "&& git commit m- \"" + CommitMessage.Text + "\"";
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.Start();
+            proc.WaitForExit();
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (folderPath.Text != "")
+            {
+                MessageBox.Show("Veuillez saisir un chemin d'accès");
+                return;
+            }
+
+            Process proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "/C cd " + folderPath.Text + "&& git push origin";
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.Start();
+            proc.WaitForExit();
         }
     }
 }
